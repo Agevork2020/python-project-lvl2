44bf03c9e797bb419ab9c0294bd639d62b21dc67
Альберт <agevork@yandex.ru>
2021-03-25 20:49:38 +0300

    2

---
+++ gendiff/__init__.py
@@ -0,0 +1,1 @@
+from gendiff.generate_diff_json import generate_diff
--- gendiff/scripts/gendiff_script.py
+++ gendiff/scripts/gendiff_script.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """A gendiff script."""
 import argparse
+from gendiff import generate_diff
 
 
 def main():
@@ -8,9 +9,12 @@ def main():
     parser = argparse.ArgumentParser(description='Generate diff')
     parser.add_argument('first_file', type=str, help='')
     parser.add_argument('second_file', type=str, help='')
+    parser.add_argument('-f', '--format', dest="format", type=str, help='set format of output')
     args = parser.parse_args()
-    print(args.indir)   
+    answer = generate_diff(args.first_file, args.second_file)
+    print(answer)   
 
 
 if __name__ == '__main__':
     main()
+
